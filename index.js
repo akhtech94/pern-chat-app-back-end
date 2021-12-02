ecba413d968cee35a06f5dd43e0b0c2c83623fd3
akhtech94 <akhtech94@gmail.com>
2021-12-02 22:41:14 +0530

    All issues fixed

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+  });
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello world!!!</h1>");
-})
+io.on('connection', (socket) => {
+  console.log(`A user with id #${socket.id} connected`);
+});
 
-app.listen(3000, () => {
-    console.log("Listening to port 3000");
-})
+server.listen(8000, () => {
+  console.log('listening on *:8000');
+});
