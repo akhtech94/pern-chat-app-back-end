ef685dec1541d2f3f6f3b785c1bcd7c0f74d5fc9
akhtech94 <akhtech94@gmail.com>
2021-12-03 15:33:12 +0530

    Implemented chat app

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
-  });
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json());
 
 io.on('connection', (socket) => {
   console.log(`A user with id #${socket.id} connected`);
+  socket.on('message', message => {
+    socket.broadcast.emit('message', message);
+  })
 });
 
 server.listen(8000, () => {
